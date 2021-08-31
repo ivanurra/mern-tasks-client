b2bdb3cfe5c338debc2ebc00eb79cd7bbd3add06
Ivan Urra <ivan.urra.wd@gmail.com>
2021-08-31 17:51:24 +0200

    routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/auth/Login";
+import NuevaCuenta from "./components/auth/NuevaCuenta";
+import Proyectos from "./components/proyectos/Proyectos";
 
 function App() {
   return (
     <div>
-      <h1>Ay, Mamá!</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
+          <Route exact path="/proyectos" component={Proyectos} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/auth/NuevaCuenta.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NuevaCuenta = () => {
+    return ( 
+        <h1>NuevaCuenta</h1>
+     );
+}
+ 
+export default NuevaCuenta;
