3f3fb6376bc8726b7d5cb79d785c525f08cce18f
Ivan Urra <ivan.urra.wd@gmail.com>
2021-09-01 07:21:48 +0200

    login component

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React from "react";
 
 const Login = () => {
-    return ( 
-        <h1>Login</h1>
-     );
-}
- 
+  const onChange = () => {};
+
+  return (
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Log In</h1>
+        <form>
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Your Email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Your Password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="campo-form">
+            <input
+              type="submit"
+              value="Log in"
+              className="btn btn-primario btn-block"
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
 export default Login;
