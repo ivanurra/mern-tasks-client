e2562af41cf5045dc28a8f1acc800cab535eaeb8
Ivan Urra <ivan.urra.wd@gmail.com>
2021-09-01 07:47:50 +0200

    State Login

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,20 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  const onChange = () => {};
+  // State para iniciar sesión
+  const [usuario, guardarUsuario] = useState({
+    email: "",
+    password: "",
+  });
+
+  // Extraemos de usuario
+  const { email, password } = usuario;
+
+  const onChange = (e) => {
+    guardarUsuario({
+      ...usuario,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // El usuario pulsa para iniciar sesión
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    // Validación
+
+    // Pasarlo al action
+  };
 
   return (
     <div className="form-usuario">
       <div className="contenedor-form sombra-dark">
         <h1>Log In</h1>
-        <form>
+        <form onSubmit={onSubmit}>
           <div className="campo-form">
             <label htmlFor="email">Email</label>
             <input
               type="email"
               id="email"
               name="email"
-              placeholder="Your Email"
+              placeholder="Your email"
+              value={email}
               onChange={onChange}
             />
           </div>
@@ -24,7 +49,8 @@ const Login = () => {
               type="password"
               id="password"
               name="password"
-              placeholder="Your Password"
+              placeholder="Your password"
+              value={password}
               onChange={onChange}
             />
           </div>
@@ -36,6 +62,10 @@ const Login = () => {
             />
           </div>
         </form>
+
+        <Link to={"/nueva-cuenta"} className="enlace-cuenta">
+          Don't have a user? Sign up here.
+        </Link>
       </div>
     </div>
   );
