107537aed0c0e7ad2f8bce0f3b22247f814e9f3f
Ivan Urra <ivan.urra.wd@gmail.com>
2021-09-19 18:40:21 +0200

    Function to show Form

--- src/components/proyectos/NuevoProyecto.js
+++ src/components/proyectos/NuevoProyecto.js
@@ -4,7 +4,7 @@ import proyectoContext from "../../context/proyectos/proyectoContext";
 const NuevoProyecto = () => {
   // Obtener el State del formulario
   const proyectosContext = useContext(proyectoContext);
-  const { formulario } = proyectosContext;
+  const { formulario, mostrarFormulario } = proyectosContext;
 
   // State para proyecto
   const [proyecto, guardarProyecto] = useState({
@@ -33,9 +33,14 @@ const NuevoProyecto = () => {
     // Reiniciar el form
   };
 
+  // Mostrar el formulario
+  const onClickFormulario = () => {
+    mostrarFormulario();
+  }
+
   return (
     <Fragment>
-      <button type="button" className="btn btn-block btn-primario">
+      <button type="button" className="btn btn-block btn-primario" onClick={onClickFormulario}>
         Nuevo Proyecto
       </button>
       {formulario ? (
--- src/context/proyectos/proyectoState.js
+++ src/context/proyectos/proyectoState.js
@@ -1,4 +1,5 @@
 import React, { useReducer } from "react";
+import { FORMULARIO_PROYECTO } from "../../types";
 import proyectoContext from "./proyectoContext";
 import proyectoReducer from "./proyectoReducer";
 
@@ -10,12 +11,18 @@ const ProyectoState = (props) => {
   // Dispatch para ejecutar las acciones
   const [state, dispatch] = useReducer(proyectoReducer, initialState);
 
-  // Serie de funciones para el CRUD
+  // Serie de funciones para el CRUD 
+  const mostrarFormulario = () => {
+    dispatch({
+      type: FORMULARIO_PROYECTO
+    })
+  }
 
   return (
     <proyectoContext.Provider
       value={{
         formulario: state.formulario,
+        mostrarFormulario
       }}
     >
       {props.children}
