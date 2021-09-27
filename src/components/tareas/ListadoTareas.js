9b41987d4794eb27788c3fde4626bb74297d729d
Ivan Urra <ivan.urra.wd@gmail.com>
2021-09-27 18:28:14 +0200

    show name project

--- src/components/tareas/ListadoTareas.js
+++ src/components/tareas/ListadoTareas.js
@@ -1,7 +1,18 @@
-import React, { Fragment } from "react";
+import React, { useContext, Fragment } from "react";
 import Tarea from "./Tarea";
+import proyectoContext from "../../context/proyectos/proyectoContext";
 
 const ListadoTareas = () => {
+  // Obtener el State de proyectos
+  const proyectosContext = useContext(proyectoContext);
+  const { proyecto } = proyectosContext;
+
+  // Si no hay proyecto seleccionado
+  if (!proyecto) return <h2>Select a project</h2>;
+
+  // Array destructuring para extraer el proyecto actual
+  const [proyectoActual] = proyecto;
+
   const tareasProyecto = [
     { nombre: "Choose Platform", estado: true },
     { nombre: "Choose Color", estado: false },
@@ -10,7 +21,7 @@ const ListadoTareas = () => {
   ];
   return (
     <Fragment>
-      <h2>Project: Virtual Shop</h2>
+      <h2>Project: {proyectoActual.nombre}</h2>
       <ul className="listado-tareas">
         {tareasProyecto.length === 0 ? (
           <li className="tarea">
