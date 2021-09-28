441860a065f8daf1318c24719af9ba7317c5aac1
Ivan Urra <ivan.urra.wd@gmail.com>
2021-09-28 07:34:55 +0200

    Hiding projects if is empty

--- src/components/tareas/FormTarea.js
+++ src/components/tareas/FormTarea.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import proyectoContext from "../../context/proyectos/proyectoContext";
 
 const FormTarea = () => {
+  // Extraer si el proyecto está activo
+  const proyectosContext = useContext(proyectoContext);
+  const { proyecto } = proyectosContext;
+
+  // Si no hay proyecto seleccionado
+  if (!proyecto) return null;
+
+  // Array destructuring para extraer el proyecto actual
+  const [proyectoActual] = proyecto;
+
   return (
     <div className="formulario">
       <form>
