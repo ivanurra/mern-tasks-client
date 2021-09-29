1bae84972a587f4ce10ebb9a63f545817ca2d794
Ivan Urra <ivan.urra.wd@gmail.com>
2021-09-29 07:23:39 +0200

    Tareas State & Provider

---
+++ src/context/tareas/tareaReducer.js
@@ -0,0 +1,9 @@
+import {} from "../../types";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
