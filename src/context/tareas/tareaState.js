e0dc94c27153d26658f5e466cde3ddc98f0b9a7f
Ivan Urra <ivan.urra.wd@gmail.com>
2021-10-04 19:37:10 +0200

    dispatch new tasks

--- src/context/tareas/tareaState.js
+++ src/context/tareas/tareaState.js
@@ -1,7 +1,7 @@
 import React, { useReducer } from "react";
 import TareaContext from "./tareaContext";
 import TareaReducer from "./tareaReducer";
-import { TAREAS_PROYECTO } from "../../types";
+import { TAREAS_PROYECTO, AGREGAR_TAREA } from "../../types";
 
 const TareaState = (props) => {
   const initialState = {
@@ -34,8 +34,16 @@ const TareaState = (props) => {
     });
   };
 
+  // Agregar una tarea al proyecto seleccionado
+  const agregarTarea = tarea => {
+    dispatch ({
+      type: AGREGAR_TAREA,
+      payload: tarea
+    })
+  }
+
   return (
-    <TareaContext.Provider value={{ tareas: state.tareas, tareasproyecto: state.tareasproyecto, obtenerTareas }}>
+    <TareaContext.Provider value={{ tareas: state.tareas, tareasproyecto: state.tareasproyecto, obtenerTareas, agregarTarea }}>
       {props.children}
     </TareaContext.Provider>
   );
