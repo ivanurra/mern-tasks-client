de28a4726fead3de56832f5eec5289ceadedcdd9
Ivan Urra <ivan.urra.wd@gmail.com>
2021-10-13 08:52:30 +0200

    install react-transition-group

--- src/components/proyectos/ListadoProyectos.js
+++ src/components/proyectos/ListadoProyectos.js
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from "react";
 import Proyecto from "./Proyecto";
 import proyectoContext from "../../context/proyectos/proyectoContext";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 const ListadoProyectos = () => {
   // Extraer proyecto de State inicial.
@@ -17,9 +18,13 @@ const ListadoProyectos = () => {
 
   return (
     <ul className="listado-proyectos">
-      {proyectos.map((proyecto) => (
-        <Proyecto key={proyecto.id} proyecto={proyecto} />
-      ))}
+      <TransitionGroup>
+        {proyectos.map((proyecto) => (
+          <CSSTransition key={proyecto.id} timeout={300} classNames="proyecto">
+            <Proyecto proyecto={proyecto} />
+          </CSSTransition>
+        ))}
+      </TransitionGroup>
     </ul>
   );
 };
